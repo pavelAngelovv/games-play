4e4a64808cf7d8c9d7603e6c32c29c786879e9b3
Pavel Angelov <pangelov@codixis.com>
2024-07-19 13:30:36 +0300

    List all games

---
+++ client/src/api/games-api.js
@@ -0,0 +1,11 @@
+import * as request from './requester'
+
+const BASE_URL = 'http://localhost:3030/jsonstore/games';
+
+export const getAll = async () => {
+    const result = await request.get(BASE_URL);
+
+    const games = Object.values(result);
+    
+    return games;
+};
--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,37 +1,22 @@
-export default function GameList () {
+import { useEffect, useState } from 'react';
+import * as gamesAPI from '../../api/games-api';
+import GameListItem from './game-list-item/GameListItem';
+
+export default function GameList() {
+    const [games, setGames] = useState([]);
+    useEffect(() => {
+        gamesAPI.getAll()
+            .then(result => setGames(result))
+    }, [])
     return (
         <section id="catalog-page">
-        <h1>All Games</h1>
-        {/* <!-- Display div: with information about every game (if any) --> */}
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
+            <h1>All Games</h1>
+            {/* <!-- Display div: with information about every game (if any) --> */}
+            {games.map(game => <GameListItem key={game._id} {...game} />)}
+            
 
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Zombie lang</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>MineCraft</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-        </div>
-
-        {/* <!-- Display paragraph: If there is no games  --> */}
-        <h3 className="no-articles">No articles yet</h3>
-    </section>
+            {/* <!-- Display paragraph: If there is no games  --> */}
+            <h3 className="no-articles">No articles yet</h3>
+        </section>
     );
 }
